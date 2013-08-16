7d1cf4cb3a40709175ac6886d460d0a111324093
David Golden <dagolden@cpan.org>
2013-08-15 21:02:07 -0400

    initial import

---
+++ lib/Class/Tiny.pm
@@ -0,0 +1,35 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package Class::Tiny;
+# ABSTRACT: No abstract given for Class::Tiny
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+=for Pod::Coverage method_names_here
+
+=head1 SYNOPSIS
+
+  use Class::Tiny;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
