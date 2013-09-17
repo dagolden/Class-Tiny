931c59e59bcd29dfbc26e880283e552c4bb137ea
David Golden <dagolden@cpan.org>
2013-09-17 06:52:46 -0400

    fix won't stay shared bug

--- lib/Class/Tiny.pm
+++ lib/Class/Tiny.pm
@@ -41,18 +41,19 @@ sub create_attributes {
 
 sub _gen_accessor {
     my ( $pkg, $name ) = @_;
-    my $default = $CLASS_ATTRIBUTES{$pkg}{$name};
+    my $outer_default = $CLASS_ATTRIBUTES{$pkg}{$name};
 
     my $sub = "sub $name { if (\@_ == 1) {";
-    if ( defined $default && ref $default eq 'CODE' ) {
+    if ( defined $outer_default && ref $outer_default eq 'CODE' ) {
         $sub .= "if ( !exists \$_[0]{$name} ) { \$_[0]{$name} = \$default->(\$_[0]) }";
     }
-    elsif ( defined $default ) {
+    elsif ( defined $outer_default ) {
         $sub .= "if ( !exists \$_[0]{$name} ) { \$_[0]{$name} = \$default }";
     }
     $sub .= "return \$_[0]{$name} } else { return \$_[0]{$name}=\$_[1] } }";
 
-    eval "package $pkg; $sub"; ## no critic
+    # default = outer_default avoids "won't stay shared" bug
+    eval "package $pkg; my \$default=\$outer_default; $sub"; ## no critic
     Carp::croak("Failed to generate attributes for $pkg: $@\n") if $@;
 }
 
